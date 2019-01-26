f3c645caa1b4f41f3880d29cdc6d065081ed695a
larakollokian <lara.kollokian@gmail.com>
2019-01-26 18:11:44 -0500

    first commit

---
+++ src/main/java/ca/mcgill/ecse428/foodme/FoodmeApplication.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse428.foodme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmeApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/ca/mcgill/ecse428/foodme/FoodmeApplicationTests.java
@@ -0,0 +1,17 @@
+package ca.mcgill.ecse428.foodme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodmeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
