3afec3791f781f56bcc1237f2cc2ebdf0f7aa93e
Ammar Rudani <aarudani7@gmail.com>
2019-02-15 20:49:37 -0500

    Created method to check if a restaurant is open

--- src/main/java/ca/mcgill/ecse428/foodme/service/Service.java
+++ src/main/java/ca/mcgill/ecse428/foodme/service/Service.java
@@ -1,8 +1,11 @@
 package ca.mcgill.ecse428.foodme.service;
-
 import org.springframework.stereotype.Repository;
-
 import ca.mcgill.ecse428.foodme.model.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
 
 @Repository
 public class Service {
@@ -21,8 +24,22 @@ public class Service {
 	}
 	
 	public boolean isRestaurantOpen(Restaurant aRestaurant) {
+
+		Date date = new Date();
+		Date time = new Date();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		Date dateInput = sdf.parse(departureDate);
+		String date1 = sdf.format(dateInput);        
+		String date2 = sdf.format(date);
+
+		SimpleDateFormat sdf2 = new SimpleDateFormat("HHmm");
+		Date timeInput = sdf2.parse(departureTime);
+		String time1 = sdf2.format(timeInput);
+		String time2 = sdf2.format(time);
 	
 		
+	
 		return true;
 	}
 }
