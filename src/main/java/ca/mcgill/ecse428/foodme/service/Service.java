f6d61b7d53e4618d60a88eeea878464dc42a9093
Ammar Rudani <aarudani7@gmail.com>
2019-02-16 18:29:55 -0500

    added javadocs for methods and added method to check if restaurant is open

--- src/main/java/ca/mcgill/ecse428/foodme/service/Service.java
+++ src/main/java/ca/mcgill/ecse428/foodme/service/Service.java
@@ -1,11 +1,10 @@
 package ca.mcgill.ecse428.foodme.service;
 
 import org.springframework.stereotype.Repository;
-
 import ca.mcgill.ecse428.foodme.model.*;
-
-
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 @Repository
@@ -35,21 +34,47 @@ public class Service {
 		return liked;
 	}
 
+	/**
+	 * Method that allows users to update their account's password
+	 * @param aUser
+	 * @param newPassword
+	 */
 	public void changePassword(User aUser, String newPassword) {
 		aUser.setPassword(newPassword);
 		return;
 		
 	}
 	
+
+	/**
+	 * Method that allows users to delete their account
+	 * @param aUser
+	 */
 	public void deleteAccount(User aUser) {
 		aUser.setCity(null);
 		aUser.setEmail(null);
 		aUser.setName(null);
 		aUser.setPassword(null);
 	}
-	
+	/**
+	 * Method that checks to see if a restaurant is open at the current time 
+	 * @param aRestaurant
+	 * @return
+	 */
 	public boolean isRestaurantOpen(Restaurant aRestaurant) {
-	
+
+		Date date = new Date();
+		Date time = new Date();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		Date dateInput = sdf.parse(departureDate);
+		String date1 = sdf.format(dateInput);        
+		String date2 = sdf.format(date);
+
+		SimpleDateFormat sdf2 = new SimpleDateFormat("HHmm");
+		Date timeInput = sdf2.parse(departureTime);
+		String time1 = sdf2.format(timeInput);
+		String time2 = sdf2.format(time);
 		
 		return true;
 	}
