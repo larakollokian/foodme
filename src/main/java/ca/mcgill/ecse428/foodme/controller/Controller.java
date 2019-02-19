fc92fe305de0b973015796225b3bccd7b6014d77
Iliana <k.iliana@live.ca>
2019-02-19 17:55:02 -0500

    search by price range attempt

--- src/main/java/ca/mcgill/ecse428/foodme/controller/Controller.java
+++ src/main/java/ca/mcgill/ecse428/foodme/controller/Controller.java
@@ -4,17 +4,14 @@ import ca.mcgill.ecse428.foodme.model.AppUser;
 import ca.mcgill.ecse428.foodme.repository.FoodmeRepository;
 import ca.mcgill.ecse428.foodme.repository.InvalidInputException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @RestController
 public class Controller 
