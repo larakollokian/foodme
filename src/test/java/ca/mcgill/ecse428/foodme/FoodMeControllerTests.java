a1f1c8cfabd6a57d71d7dab6c94a61fbacd560e4
hmarine <marine.huynh@yahoo.ca>
2019-02-24 21:22:32 -0500

    test AllLiked + controllerTest Ignore

--- src/test/java/ca/mcgill/ecse428/foodme/FoodMeControllerTests.java
+++ src/test/java/ca/mcgill/ecse428/foodme/FoodMeControllerTests.java
@@ -1,16 +1,21 @@
 package ca.mcgill.ecse428.foodme;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import ca.mcgill.ecse428.foodme.controller.Controller;
@@ -18,8 +23,11 @@ import ca.mcgill.ecse428.foodme.repository.FoodmeRepository;
 import ca.mcgill.ecse428.foodme.service.AuthenticationService;
 import io.restassured.RestAssured;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import io.restassured.response.Response;
 
+
+/*
+ * This class is still in progress, nothing in here should affect the code
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class FoodMeControllerTests {
@@ -39,7 +47,7 @@ public class FoodMeControllerTests {
     @Autowired
 	private MockMvc mockMvc;
     
-    @MockBean 
+    @Mock 
     FoodmeRepository repository;
 	
 //	@Mock
@@ -49,9 +57,11 @@ public class FoodMeControllerTests {
     public void setup() throws Exception {
     	RestAssured.baseURI="http://localhost/";
     	RestAssured.port=8080;
+		this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
     
-    @Test
+   // @Test
+    @Ignore
     public void testControllerListAllLiked() throws Exception {
     	String username = "Marine";
 
@@ -62,5 +72,24 @@ public class FoodMeControllerTests {
     	get("/users/{user}/allliked/").
     	then().
     	statusCode(HttpStatus.OK.value());
+    }
+    
+    @Ignore
+    //@Test
+    public void testControllerListAllLiked2() throws Exception {
+    	String username = "Marine";
+
+    	mockMvc.perform(MockMvcRequestBuilders.get("/users/{user}/allliked/", username)
+    			.accept(MediaType.ALL_VALUE))
+    			.andExpect(status().isOk());
+    	
+    	
+//    	RestAssuredMockMvc.given().
+//    	param("user", username).
+//    	log().all().
+//    	when().
+//    	get("/users/{user}/allliked/").
+//    	then().
+//    	statusCode(HttpStatus.OK.value());
     }
 }
