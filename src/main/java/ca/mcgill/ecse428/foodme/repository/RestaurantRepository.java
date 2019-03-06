5fd706ee76b4e924a3b51cf09db3b21072697224
Ammar Rudani <aarudani7@gmail.com>
2019-03-05 21:01:16 -0500

    Update RestaurantRepository.java

--- src/main/java/ca/mcgill/ecse428/foodme/repository/RestaurantRepository.java
+++ src/main/java/ca/mcgill/ecse428/foodme/repository/RestaurantRepository.java
@@ -115,6 +115,24 @@ public class RestaurantRepository {
 		return restaurant;
 	}
 
+
+	public Restaurant deleteRestaurant(String restaurantID, String restaurantName) throws InvalidInputException {
+		if(restaurantID.length() == 0 || restaurantName.length() == 0){
+			throw new InvalidInputException("restaurantID and restaurantName must be at least 1 character");
+		}
+		else if (entityManager.find(Restaurant.class,restaurantID) == null){
+			throw new InvalidInputException("Restaurant does not exists");
+		}
+		else {
+			Restaurant restaurant =  entityManager.find(Restaurant.class, restaurantID);
+			entityManager.remove(restaurant);
+			
+			return restaurant;
+
+		}
+		
+	}
+
 	/**
 	 * Method that adds a restaurant and a user to the likedRestaurant list in the database
 	 * @param username
