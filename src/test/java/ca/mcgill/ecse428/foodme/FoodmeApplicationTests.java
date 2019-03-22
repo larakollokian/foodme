7a6a0748eda3a197bcb05db5325029fc059d64dc
erionhysa <erionhysa@hotmail.com>
2019-03-22 11:35:21 -0400

    removed duplicate test

--- src/test/java/ca/mcgill/ecse428/foodme/FoodmeApplicationTests.java
+++ src/test/java/ca/mcgill/ecse428/foodme/FoodmeApplicationTests.java
@@ -704,29 +704,6 @@ public class FoodmeApplicationTests {
         }
     }
 
-    /**
-     * Test UT for removing a restaurant from the disliked list
-     * @throws InvalidInputException
-     */
-    @Test
-    public void testRemoveDisliked () {
-        String restaurant_id = "RIIOjIdlzRyESw1BkmQHtw";
-        String restaurant_name = "Tacos Et Tortas";
-
-        Restaurant restaurant = new Restaurant();
-        restaurant.setRestaurantID(restaurant_id);
-        restaurant.setRestaurantName(restaurant_name);
-
-        try {
-            AppUser user =	appUserRepository.createAccount(USERNAME, FIRSTNAME, LASTNAME, EMAIL, PASSWORD);
-            when(restaurantRepository.createRestaurant(restaurant_id,restaurant_name)).thenReturn(restaurant);
-            when(restaurantRepository.removeDisliked(user.getUsername(),restaurant_id)).thenReturn(restaurant);
-            assertEquals(restaurantRepository.removeDisliked(user.getUsername(),restaurant_id),restaurant);
-            Mockito.verify(restaurantRepository).removeDisliked(user.getUsername(),restaurant_id);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Test UT for adding a restaurant to the visited list
