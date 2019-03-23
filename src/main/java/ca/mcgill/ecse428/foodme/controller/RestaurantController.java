d1e2155fbb3bec342d40c32a7fb534586bf57eb0
Marine Huynh <marine.huynh@yahoo.ca>
2019-03-23 15:06:56 -0400

    test passes, 500

--- src/main/java/ca/mcgill/ecse428/foodme/controller/RestaurantController.java
+++ src/main/java/ca/mcgill/ecse428/foodme/controller/RestaurantController.java
@@ -1,15 +1,27 @@
 package ca.mcgill.ecse428.foodme.controller;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import ca.mcgill.ecse428.foodme.exception.NullObjectException;
 import ca.mcgill.ecse428.foodme.model.Response;
@@ -23,6 +35,8 @@ public class RestaurantController {
 
     @Autowired
     RestaurantRepository restaurantRepository;
+    
+    String APIKey = "F5ByVWSif5NWb6w3YYAQjRGOI9Xcg8WKqzBDkPnEl4YDneNpsaKn35YcFEqJyvyV_kUTStuTG2n9-Pi9R7-u9GIkmBQY8LjfNJSrAVEs_K5pGJLCAsWc4N3oxGRgXHYx";
 
     /**
      * Greeting
--- src/main/java/ca/mcgill/ecse428/foodme/controller/SearchController.java
+++ src/main/java/ca/mcgill/ecse428/foodme/controller/SearchController.java
@@ -1,254 +1,315 @@
 package ca.mcgill.ecse428.foodme.controller;
 
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Random;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import ca.mcgill.ecse428.foodme.model.Response;
 
-import java.util.Random;
-
-import static org.springframework.web.client.HttpClientErrorException.*;
-
 @RestController
 @RequestMapping("/search")
 public class SearchController {
 
-    String APIKey = "F5ByVWSif5NWb6w3YYAQjRGOI9Xcg8WKqzBDkPnEl4YDneNpsaKn35YcFEqJyvyV_kUTStuTG2n9-Pi9R7-u9GIkmBQY8LjfNJSrAVEs_K5pGJLCAsWc4N3oxGRgXHYx";
-    String googleApiKey = "AIzaSyAbDiuDSRG-3oyFUzlS0SOy1g5b0n49dus";
+	String APIKey = "F5ByVWSif5NWb6w3YYAQjRGOI9Xcg8WKqzBDkPnEl4YDneNpsaKn35YcFEqJyvyV_kUTStuTG2n9-Pi9R7-u9GIkmBQY8LjfNJSrAVEs_K5pGJLCAsWc4N3oxGRgXHYx";
+	String googleApiKey = "AIzaSyAbDiuDSRG-3oyFUzlS0SOy1g5b0n49dus";
 
-    
-    /**
-     * Greeting
-     * @return Search connected
-     */
-    @RequestMapping("/")
-    public String greeting() {
-        return "Search connected!";
-    }
+	/**
+	 * Greeting
+	 * @return Search connected
+	 */
+	@RequestMapping("/")
+	public String greeting() {
+		return "Search connected!";
+	}
 
-    //Template
-    public ResponseEntity<String> getMapping(String url) throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + APIKey);
-        headers.setContentType(MediaType.APPLICATION_JSON);
+	//Template
+	public ResponseEntity<String> getMapping(String url) throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + APIKey);
+		headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
+		HttpEntity<Void> entity = new HttpEntity<>(headers);
 
-        // Response
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response;
+		// Response
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response;
 
-        try {
-            response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-            return response;
-        } catch (HttpClientErrorException e){
-            throw new Exception("Something went wrong! Please make sure you've put in the right information!");
-        }
-    }
-
-    /**
-     * Controller method that calls the API to return a restaurant based on its id
-     * @param id
-     * @return Restaurant and all its information associated with it
-     * @throws Exception
-     */
-    @GetMapping("/businesses/")
-    public ResponseEntity<String> lookUpRestaurantByID (
-    		@RequestParam("id") String id) throws Exception{
-
-        // Set up url
-        String url = "https://api.yelp.com/v3/businesses/" + id;
-        return getMapping(url);
-    }
-
-    /**
-     * Method that searches restaurants based on type of cuisine, must select from the list of cuisines available in the yelp API
-     * Due to the API's limits we can only return restaurants that currently have a review
-     * @param cuisine
-     * @param location
-     * @return
-     * @throws Exception
-     */
-    @GetMapping("/cuisine/")
-    public ResponseEntity<String> searchByCuisine (
-            @RequestParam("location") String location,
-            @RequestParam("cuisine") String cuisine) throws Exception{
-
-        // Set up url
-        String url = "https://api.yelp.com/v3/businesses/search?location=" + location + "&cuisine=" + cuisine;
-        return getMapping(url);
-    }
-
-    /**
-     * Method that searches restaurants based on type of cuisine, must select from the list of cuisines available in the yelp API
-     * Due to the API's limits we can only return restaurants that currently have a review
-     * @param cuisine
-     * @param longitude
-     * @param latitude
-     * @return
-     * @throws Exception
-     */
-    @GetMapping("/cuisine/longitude/latitude/")
-    public ResponseEntity<String> searchByCuisine (
-            @RequestParam("longitude") String longitude,
-            @RequestParam("latitude") String latitude,
-            @RequestParam("cuisine") String cuisine) throws Exception{
-
-        // Set up url
-        String url = "https://api.yelp.com/v3/businesses/search?longitude=" + longitude + "&latitude=" + latitude + "&cuisine=" + cuisine;
-        return getMapping(url);
-    }
-
-    /**
-     * Method that searches restaurants using google API
-     *
-     * @param location
-     * @return ResponseEntity
-     * @throws Exception
-     */
-    @GetMapping("/google/{location}/")
-    public ResponseEntity<String> searchGoogle(
-            @PathVariable("location") String location) {
-
-        // Set up url
-        String query = "restaurants+in+" + location;
-        String url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + googleApiKey;
-
-        HttpEntity<Void> entity = null;
-
-        // Response
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-
-        return response;
-    }
-
-    /**
-     * Method that searches restaurant and sort them by best_match, rating, review_count or distance
-     * If recommend param is set to 1, it will return random restaurant from the result
-     *
-     * @param location
-     * @param sortby:    best_match, rating, review_count or distance
-     * @param recommend: 1-> True, 0-> False
-     * @return ResponseEntity
-     * @throws Exception
-     */
-    @GetMapping("/{location}/{sortby}/{recommend}/")
-    public ResponseEntity<String> searchSortByDistance(
-            @PathVariable("location") String location,
-            @PathVariable("sortby") String sortby,
-            @PathVariable("recommend") int recommend) throws Exception {
-        // Set up url
-        String url = null;
-        String extraParam = "";
-
-        if (recommend == 1) {
-            Random rand = new Random();
-            int randomOffset = rand.nextInt(50);
-            String offset = Integer.toString(randomOffset);
-            extraParam = extraParam + "&offset=" + offset + "&limit=1";
-        }
-
-        url = "https://api.yelp.com/v3/businesses/search?location=" + location + "&sort_by=" + sortby + extraParam;
-
-        return getMapping(url);
-    }
-
-    /**
-     * Method that searches restaurant and sort them by best_match, rating, review_count or distance
-     * If recommend param is set to 1, it will return random restaurant from the result
-     *
-     * @param sortby:    best_match, rating, review_count or distance
-     * @param recommend: 1-> True, 0-> False
-     * @return ResponseEntity
-     * @throws Exception
-     */
-    @GetMapping("/{sortby}/{recommend}/")
-    public ResponseEntity<String> searchByLongLat(
-            @RequestParam("long") String longitude,
-            @RequestParam("lat") String latitude,
-            @PathVariable("sortby") String sortby,
-            @PathVariable("recommend") int recommend) throws Exception {
-        // Set up url
-        String url = null;
-        String extraParam = "";
-
-        if (recommend == 1) {
-            Random rand = new Random();
-            int randomOffset = rand.nextInt(50);
-            String offset = Integer.toString(randomOffset);
-            extraParam = extraParam + "&offset=" + offset + "&limit=1";
-        }
-
-        url = "https://api.yelp.com/v3/businesses/search?longitude=" + longitude
-                    + "&latitude=" + latitude
-                    + "&sort_by=" + sortby + extraParam;
-
-        return getMapping(url);
-    }
-
-    /**
-     * Method that searches restaurant based on price range. Must include either location or longitude and latitude.
-     *
-     * @param location
-     * @param price
-     * @return
-     * @throws Exception
-     */
-    @GetMapping("/price/")
-    public ResponseEntity<String> searchByPriceRange(
-            @RequestParam("location") String location,
-            @RequestParam("price") String price) throws Exception {
-
-        // Set up url
-        String url = "https://api.yelp.com/v3/businesses/search?location=" + location + "&price=" + price;
-
-        return getMapping(url);
-    }
-
-    /**
-     * Method that searches restaurant based on price range. Must include either location or longitude and latitude.
-     *
-     * @param longitude
-     * @param latitude
-     * @param price
-     * @return
-     * @throws Exception
-     */
-    @GetMapping("/price/longitude/latitude/")
-    public ResponseEntity<String> searchByPriceRange(
-            @RequestParam("longitude") String longitude,
-            @RequestParam("latitude") String latitude,
-            @RequestParam("price") String price) throws Exception {
-
-        // Set up url
-        String url = "https://api.yelp.com/v3/businesses/search?longitude=" + longitude + "&latitude=" + latitude + "&price=" + price;
-
-        return getMapping(url);
-    }
-    
-    //TODO Marine
-    @GetMapping("/businesses/{id}/closing")
-    public ResponseEntity<String> isRestaurantClosingInOneHour(@RequestParam("id")String id){
-        // Set up url
-        String url = "https://api.yelp.com/v3/businesses/" + id;
-        try {
-			ResponseEntity<String> answer = getMapping(url);
-			answer.getBody();
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		try {
+			response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+			return response;
+		} catch (HttpClientErrorException e){
+			throw new Exception("Something went wrong! Please make sure you've put in the right information!");
 		}
-    	
-    	return new ResponseEntity<String>("The restaurant is closing in less than 1 hour.", HttpStatus.OK);
+	}
 
-    	//want this type of answer with true or false
-    	//{\"response\":true,\"message\":\"User account successfully deleted.\"}
-    	
-    			//ResponseEntity.ok(gson.ToJson(""));
-    			//ResponseEntity.status(HttpStatus.OK).body(new Response(true, "The restaurant is closing in less than 1 hour."));
-    }
+	/**
+	 * Controller method that calls the API to return a restaurant based on its id
+	 * @param id
+	 * @return Restaurant and all its information associated with it
+	 * @throws Exception
+	 */
+	@GetMapping("/businesses/")
+	public ResponseEntity<String> lookUpRestaurantByID (
+			@RequestParam("id") String id) throws Exception{
+
+		// Set up url
+		String url = "https://api.yelp.com/v3/businesses/" + id;
+		return getMapping(url);
+	}
+
+	/**
+	 * Method that searches restaurants based on type of cuisine, must select from the list of cuisines available in the yelp API
+	 * Due to the API's limits we can only return restaurants that currently have a review
+	 * @param cuisine
+	 * @param location
+	 * @return
+	 * @throws Exception
+	 */
+	@GetMapping("/cuisine/")
+	public ResponseEntity<String> searchByCuisine (
+			@RequestParam("location") String location,
+			@RequestParam("cuisine") String cuisine) throws Exception{
+
+		// Set up url
+		String url = "https://api.yelp.com/v3/businesses/search?location=" + location + "&cuisine=" + cuisine;
+		return getMapping(url);
+	}
+
+	/**
+	 * Method that searches restaurants based on type of cuisine, must select from the list of cuisines available in the yelp API
+	 * Due to the API's limits we can only return restaurants that currently have a review
+	 * @param cuisine
+	 * @param longitude
+	 * @param latitude
+	 * @return
+	 * @throws Exception
+	 */
+	@GetMapping("/cuisine/longitude/latitude/")
+	public ResponseEntity<String> searchByCuisine (
+			@RequestParam("longitude") String longitude,
+			@RequestParam("latitude") String latitude,
+			@RequestParam("cuisine") String cuisine) throws Exception{
+
+		// Set up url
+		String url = "https://api.yelp.com/v3/businesses/search?longitude=" + longitude + "&latitude=" + latitude + "&cuisine=" + cuisine;
+		return getMapping(url);
+	}
+
+	/**
+	 * Method that searches restaurants using google API
+	 *
+	 * @param location
+	 * @return ResponseEntity
+	 * @throws Exception
+	 */
+	@GetMapping("/google/{location}/")
+	public ResponseEntity<String> searchGoogle(
+			@PathVariable("location") String location) {
+
+		// Set up url
+		String query = "restaurants+in+" + location;
+		String url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + googleApiKey;
+
+		HttpEntity<Void> entity = null;
+
+		// Response
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+		return response;
+	}
+
+	/**
+	 * Method that searches restaurant and sort them by best_match, rating, review_count or distance
+	 * If recommend param is set to 1, it will return random restaurant from the result
+	 *
+	 * @param location
+	 * @param sortby:    best_match, rating, review_count or distance
+	 * @param recommend: 1-> True, 0-> False
+	 * @return ResponseEntity
+	 * @throws Exception
+	 */
+	@GetMapping("/{location}/{sortby}/{recommend}/")
+	public ResponseEntity<String> searchSortByDistance(
+			@PathVariable("location") String location,
+			@PathVariable("sortby") String sortby,
+			@PathVariable("recommend") int recommend) throws Exception {
+		// Set up url
+		String url = null;
+		String extraParam = "";
+
+		if (recommend == 1) {
+			Random rand = new Random();
+			int randomOffset = rand.nextInt(50);
+			String offset = Integer.toString(randomOffset);
+			extraParam = extraParam + "&offset=" + offset + "&limit=1";
+		}
+
+		url = "https://api.yelp.com/v3/businesses/search?location=" + location + "&sort_by=" + sortby + extraParam;
+
+		return getMapping(url);
+	}
+
+	/**
+	 * Method that searches restaurant and sort them by best_match, rating, review_count or distance
+	 * If recommend param is set to 1, it will return random restaurant from the result
+	 *
+	 * @param sortby:    best_match, rating, review_count or distance
+	 * @param recommend: 1-> True, 0-> False
+	 * @return ResponseEntity
+	 * @throws Exception
+	 */
+	@GetMapping("/{sortby}/{recommend}/")
+	public ResponseEntity<String> searchByLongLat(
+			@RequestParam("long") String longitude,
+			@RequestParam("lat") String latitude,
+			@PathVariable("sortby") String sortby,
+			@PathVariable("recommend") int recommend) throws Exception {
+		// Set up url
+		String url = null;
+		String extraParam = "";
+
+		if (recommend == 1) {
+			Random rand = new Random();
+			int randomOffset = rand.nextInt(50);
+			String offset = Integer.toString(randomOffset);
+			extraParam = extraParam + "&offset=" + offset + "&limit=1";
+		}
+
+		url = "https://api.yelp.com/v3/businesses/search?longitude=" + longitude
+				+ "&latitude=" + latitude
+				+ "&sort_by=" + sortby + extraParam;
+
+		return getMapping(url);
+	}
+
+	/**
+	 * Method that searches restaurant based on price range. Must include either location or longitude and latitude.
+	 *
+	 * @param location
+	 * @param price
+	 * @return
+	 * @throws Exception
+	 */
+	@GetMapping("/price/")
+	public ResponseEntity<String> searchByPriceRange(
+			@RequestParam("location") String location,
+			@RequestParam("price") String price) throws Exception {
+
+		// Set up url
+		String url = "https://api.yelp.com/v3/businesses/search?location=" + location + "&price=" + price;
+
+		return getMapping(url);
+	}
+
+	/**
+	 * Method that searches restaurant based on price range. Must include either location or longitude and latitude.
+	 *
+	 * @param longitude
+	 * @param latitude
+	 * @param price
+	 * @return
+	 * @throws Exception
+	 */
+	@GetMapping("/price/longitude/latitude/")
+	public ResponseEntity<String> searchByPriceRange(
+			@RequestParam("longitude") String longitude,
+			@RequestParam("latitude") String latitude,
+			@RequestParam("price") String price) throws Exception {
+
+		// Set up url
+		String url = "https://api.yelp.com/v3/businesses/search?longitude=" + longitude + "&latitude=" + latitude + "&price=" + price;
+
+		return getMapping(url);
+	}
+
+	/**
+	 * Method that searches a restaurant closing hours and checks if the restaurant will be closing within one hour from now
+	 * @param id id of the restaurant
+	 * @return true when it is closing within one hour
+	 * @return false when it is not closing within one hour
+	 * @throws Exception
+	 */
+	@GetMapping("/get/closing")
+	public ResponseEntity searchRestaurantClosingInOneHour(
+			@RequestParam("id") String id) throws Exception {
+
+		// Set up url
+		String url = "https://api.yelp.com/v3/businesses/" + id;
+		ResponseEntity<String> response = getMapping(url);
+		String contentAsString = response.getBody();
+
+		String endTime = "";
+		LocalDate date = LocalDate.now();
+		LocalTime time = LocalTime.now();
+
+		DayOfWeek dayOfWeek = date.getDayOfWeek();
+		int dayOfWeekInInteger = 0;
+
+		//convert to the day
+		switch(dayOfWeek) {
+		case MONDAY: dayOfWeekInInteger = 0;
+		break;
+		case TUESDAY: dayOfWeekInInteger = 1;
+		break;
+		case WEDNESDAY: dayOfWeekInInteger = 2;
+		break;
+		case THURSDAY: dayOfWeekInInteger = 3;
+		break;
+		case FRIDAY: dayOfWeekInInteger = 4;
+		break;
+		case SATURDAY: dayOfWeekInInteger = 5;
+		break;
+		case SUNDAY: dayOfWeekInInteger = 6;
+		break;
+		default: dayOfWeekInInteger = 0;
+		break;
+		}
+
+		JSONObject json = new JSONObject(contentAsString);
+		JSONArray hours = json.getJSONArray("hours");
+		int length = hours.length();
+
+		for(int i =0 ; i<length; i++){
+			JSONObject allOpeningHours = hours.getJSONObject(i);
+			JSONArray open = allOpeningHours.getJSONArray("open");
+			int days = open.length();
+			for(int j=0; j<days; j++) {
+				JSONObject json3 = open.getJSONObject(j);
+				int day = json3.getInt("day");
+
+				//check if the same DAY in int
+				if(dayOfWeekInInteger==day) {
+					endTime = json3.getString("end");
+					String hour = endTime.toString().substring(0, 2);
+					String minute = endTime.toString().substring(2, 4);
+					LocalTime restaurantTime = LocalTime.of(Integer.parseInt(hour), Integer.parseInt(minute));
+
+					restaurantTime.minusHours(1);
+					if(time.isAfter(restaurantTime)) {
+						return ResponseEntity.status(HttpStatus.OK).body(new Response(true, "The restaurant is closing in less than 1 hour."));
+					}
+				}
+			}
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(new Response(false, "The restaurant is still open."));
+	}
+
 }
